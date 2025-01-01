e892a3ba49f29ba2328cc63b14f48e2c11e75401
failedtocode <mahendradew1952@gmail.com>
2025-01-01 18:22:15 +0530

    redirect url in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout ({
 }>) {
   return (
     <ClerkProvider
-    afterSignOutUrl='/'
+    afterSignOutUrl='/abcd'
     >
       <Providers>
         <html lang='en' suppressHydrationWarning>
